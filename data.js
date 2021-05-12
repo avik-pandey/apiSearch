352a6e00ca5f9d47a7b7ef7eec5098be909391cd
Avik Pandey <avikmika@gmail.com>
2021-05-12 14:23:33 +0530

    ok

--- data.js
+++ data.js
@@ -2,47 +2,121 @@ const fs = require('fs')
 const csv = require('csv-parser')
 const randomWords = require('random-words')
 
-articles = [
-{name:'i am avik',surname:'i am pandey'},
-{name:'this is i am',surname:'yeah right'},
-{name:'this is not right',surname:'cool'}
-];
+articles = [];
 fin = [];
+allData = [];
 string_to_array = function (str) {
     return str.trim().split(" ");
 };
-for(var i = 0;i<articles.length;i++){
-    var s = articles[i].name;
-    
-    var arr = string_to_array(s);
-    var mp1 = new Map();
-
-    for(var j=0;j<arr.length;j++)mp1.set(arr[j],1);
-    var arr2 = [];
-    for(var j = 0;j<articles.length;j++){
-        if(j == i)continue;
-        var arr1 = string_to_array(articles[j].name);
-        var cnt = 0;
-        for(var k = 0;k<arr1.length;k++){
-            if(mp1.has(arr1[k]) == true){
-                cnt++;
-            }
+function data(){
+fs.createReadStream('IndianFinancialNews.csv')
+      .pipe(csv())
+      .on('data', function (row) {
+        const art = {
+          heading : row.Title,
+          description : row.Description
         }
-       arr2.push([cnt,j]);
-       
+        articles.push(art);
+      })
+      .on('end', function () {
+        console.log(articles[0].heading);
+        
+        for(var i = 0;i<10000;i++){
+            var s = articles[i].heading;
+            // console.log(s);
+            var arr = string_to_array(s);
+            // console.log(arr);
+            var mp1 = new Map();
+        
+            for(var j=0;j<arr.length;j++)mp1.set(arr[j],1);
+            var arr2 = [];
+            for(var j = 0;j<10000;j++){
+                if(j == i)continue;
+                var arr1 = string_to_array(articles[j].heading);
+                var cnt = 0;
+                for(var k = 0;k<arr1.length;k++){
+                    if(mp1.has(arr1[k]) == true){
+                        cnt++;
+                    }
+                }
+               arr2.push([cnt,j]);
+               
+            }
+            arr2.sort();
+            arr2.reverse();
+            var k = arr2[0][0];
+            var arts = [];
+            arts.push(articles[i]);
+            for(var j = 0;j<arr2.length;j++){
+                if(k == arr2[j][0])arts.push(articles[arr2[j][1]]);
+                else break;
+            }
+            fin.push(arts);
+        
+        }
+        // console.log("test");
+        // console.log(fin);
+        
+        for(var i  =0;i<fin.length;i++){
+            const finArt = {
+                heading :articles[i].heading,
+                description:articles[i].description,
+                groups:fin[i]
+            }
+            allData.push(finArt);
+        }
+        // console.log(allData);
+      })
     }
-    arr2.sort();
-    arr2.reverse();
-    var k = arr2[0][0];
-    var arts = [];
-    arts.push(articles[i]);
-    for(var j = 0;j<arr2.length;j++){
-        if(k == arr2[j][0])arts.push(articles[arr2[j][1]]);
-        else break;
-    }
-    fin.push(arts);
+    // var n = 30000;
+    // setInterval(function(){
 
-}
-console.log(fin);
+    //     data();
+          
+    //   }, n * 1000);   
+    data();
+module.exports = allData;
+// articles = [
+// {name:'i am avik',surname:'i am pandey'},
+// {name:'this is i am',surname:'yeah right'},
+// {name:'this is not right',surname:'cool'}
+// ];
+// fin = [];
+// string_to_array = function (str) {
+//     return str.trim().split(" ");
+// };
+// for(var i = 0;i<articles.length;i++){
+//     var s = articles[i].name;
+    
+//     var arr = string_to_array(s);
+//     var mp1 = new Map();
+
+//     for(var j=0;j<arr.length;j++)mp1.set(arr[j],1);
+//     var arr2 = [];
+//     for(var j = 0;j<articles.length;j++){
+//         if(j == i)continue;
+//         var arr1 = string_to_array(articles[j].name);
+//         var cnt = 0;
+//         for(var k = 0;k<arr1.length;k++){
+//             if(mp1.has(arr1[k]) == true){
+//                 cnt++;
+//             }
+//         }
+//        arr2.push([cnt,j]);
+       
+//     }
+//     arr2.sort();
+//     arr2.reverse();
+//     var k = arr2[0][0];
+//     var arts = [];
+//     arts.push(articles[i]);
+//     for(var j = 0;j<arr2.length;j++){
+//         if(k == arr2[j][0])arts.push(articles[arr2[j][1]]);
+//         else break;
+//     }
+//     fin.push(arts);
+
+// }
+
 
 
--- routes.js
+++ routes.js
@@ -5,12 +5,27 @@ const bodyParser = require('body-parser').json();
 const fs = require('fs')
 const csv = require('csv-parser')
 const randomWords = require('random-words')
-
+const dataset = require('./data');
+// console.log(dataSet);
 // 
 var client = new elastic.Client({
   host: 'localhost:9200',
   log: 'trace'
 });
+
+
+// client.indices.delete({
+//   index:'dataset'
+// },function(err,resp){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("deleted");
+//     console.log("11111");
+//   }
+// })
+
 async function f1(req,res){
     let temp = await client.ping({
       // ping usually has a 3000ms timeout
@@ -31,122 +46,138 @@ async function f1(req,res){
     else{
       console.log("1");
     }
-    no = [{phone:'99107'},{phone:'98111'},{phone:'78211'}];
-    articles = [];
-    let temp1 = await fs.createReadStream('IndianFinancialNews.csv')
-      .pipe(csv())
-      .on('data', function (row) {
-        const art = {
-          heading : row.Title,
-          description : row.Description
-        }
-        articles.push(art);
-      })
-      .on('end', function () {
-        // console.log(articles[0]);
-      })
-     
-      if(temp1.err){
-        console.log(err);
-      }
-      else{
-        // console.log(articles[0]);
-        console.log("2");
-      }
-
-      let temp2 = client.indices.create({
-        index:'no',
+   
     
-    },function(err,resp,status){
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("ok");
-            console.log("create",resp);
-            console.log(no[0]);
-        }
-    
-    });
 
-    if(temp2.err){
-      console.log(err)
-    }
-    else{
-      console.log(3);
+    //   let temp2 = client.index({
+    //     index:'dataset',
+    
+    // },function(err,resp,status){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         console.log("ok");
+    //         console.log("create",resp);
+    //         console.log(dataset[0]);
+    //     }
+    
+    // });
+
+    // if(temp2.err){
+    //   console.log(err)
+    // }
+    // else{
+    //   console.log(3);
+    // }
+    for(var i=0;i<dataset.length;i++){
+       client.index({
+        index:'dataset',
+        body:dataset[i]
+      },function(err,res){
+        if(err)console.log(err)
+        else console.log("done !");
+      })
+      if(i==0)console.log(dataset[i]);
     }
     
 }
+var n = 1200;
+    setInterval(function(){
+
+        // f1();
+          
+      }, n * 1000);  
 // f1();
 
+client.indices.exists({
+  index:'dataset'
+}).then(function(resp){
+  console.log("yes")
+})
+.catch(err=>{
+  console.log(err);
+})
 
 async function f2(req,res){
-    const {temp} = await client.search({
-      index: 'no',
+    
+    const {temp} = await client.search({  
+      index: 'dataset',
+      
       body: {
         query: {
-          match: {
-            phone: '99107'
-          }
+          match: { heading: 'ATMs to become virtual bank branches, accept deposits with instant credit' }
         }
       }
-    })
+    },function (error, response,status) {
+        if (error){
+          console.log("search error: "+error)
+        }
+        else {
+          console.log("--- Response ---");
+          console.log(response);
+          console.log("--- Hits ---");
+          response.hits.hits.forEach(function(hit){
+            console.log(hit);
+          })
+        }
+    });
 
-    
-      console.log(temp.hits.hits)
-      console.log(1);
+    console.log(temp); 
+      // console.log(temp.hits.hits)
+      // console.log(1);
     
 
 }
-// f2().catch(console.log);
+f2().catch(console.log);
 
 
-async function run () {
-  // Let's start by indexing some data
-  await client.index({
-    index: 'game-of-thrones',
-    body: {
-      character: 'Ned Stark',
-      quote: 'Winter is coming.'
-    }
-  })
+// async function run () {
+//   // Let's start by indexing some data
+//   await client.index({
+//     index: 'game-of-thrones',
+//     body: {
+//       character: 'Ned Stark',
+//       quote: 'Winter is coming.'
+//     }
+//   })
 
-  await client.index({
-    index: 'game-of-thrones',
-    body: {
-      character: 'Daenerys Targaryen',
-      quote: 'I am the blood of the dragon.'
-    }
-  })
+//   await client.index({
+//     index: 'game-of-thrones',
+//     body: {
+//       character: 'Daenerys Targaryen',
+//       quote: 'I am the blood of the dragon.'
+//     }
+//   })
 
-  await client.index({
-    index: 'game-of-thrones',
-    // here we are forcing an index refresh,
-    // otherwise we will not get any result
-    // in the consequent search
-    refresh: true,
-    body: {
-      character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.'
-    }
-  })
+//   await client.index({
+//     index: 'game-of-thrones',
+//     // here we are forcing an index refresh,
+//     // otherwise we will not get any result
+//     // in the consequent search
+//     refresh: true,
+//     body: {
+//       character: 'Tyrion Lannister',
+//       quote: 'A mind needs books like a sword needs a whetstone.'
+//     }
+//   })
 
-  // Let's search!
-  const { body } = await client.search({
-    index: 'game-of-thrones',
-    body: {
-      query: {
-        match: {
-          quote: 'winter'
-        }
-      }
-    }
-  })
+//   // Let's search!
+//   const { body } = await client.search({
+//     index: 'game-of-thrones',
+//     body: {
+//       query: {
+//         match: {
+//           quote: 'winter'
+//         }
+//       }
+//     }
+//   })
 
-  console.log(body)
-}
+//   console.log(body)
+// }
 
-run().catch(console.log)
+// run().catch(console.log)
 // client.ping({
 //   // ping usually has a 3000ms timeout
 //   requestTimeout: Infinity,
