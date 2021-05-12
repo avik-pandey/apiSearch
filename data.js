d0c1af4b1bc80c5d36277d400a2550c6750649f5
Avik Pandey <avikmika@gmail.com>
2021-05-12 14:31:11 +0530

    ok

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ fs.createReadStream('IndianFinancialNews.csv')
         articles.push(art);
       })
       .on('end', function () {
-        console.log(articles[0].heading);
+        // algorithm to group data
         
         for(var i = 0;i<10000;i++){
             var s = articles[i].heading;
@@ -68,55 +68,10 @@ fs.createReadStream('IndianFinancialNews.csv')
         // console.log(allData);
       })
     }
-    // var n = 30000;
-    // setInterval(function(){
-
-    //     data();
-          
-    //   }, n * 1000);   
+    
     data();
 module.exports = allData;
-// articles = [
-// {name:'i am avik',surname:'i am pandey'},
-// {name:'this is i am',surname:'yeah right'},
-// {name:'this is not right',surname:'cool'}
-// ];
-// fin = [];
-// string_to_array = function (str) {
-//     return str.trim().split(" ");
-// };
-// for(var i = 0;i<articles.length;i++){
-//     var s = articles[i].name;
-    
-//     var arr = string_to_array(s);
-//     var mp1 = new Map();
 
-//     for(var j=0;j<arr.length;j++)mp1.set(arr[j],1);
-//     var arr2 = [];
-//     for(var j = 0;j<articles.length;j++){
-//         if(j == i)continue;
-//         var arr1 = string_to_array(articles[j].name);
-//         var cnt = 0;
-//         for(var k = 0;k<arr1.length;k++){
-//             if(mp1.has(arr1[k]) == true){
-//                 cnt++;
-//             }
-//         }
-//        arr2.push([cnt,j]);
-       
-//     }
-//     arr2.sort();
-//     arr2.reverse();
-//     var k = arr2[0][0];
-//     var arts = [];
-//     arts.push(articles[i]);
-//     for(var j = 0;j<arr2.length;j++){
-//         if(k == arr2[j][0])arts.push(articles[arr2[j][1]]);
-//         else break;
-//     }
-//     fin.push(arts);
-
-// }
 
 
 
--- routes.js
+++ routes.js
@@ -13,24 +13,24 @@ var client = new elastic.Client({
   log: 'trace'
 });
 
-
-// client.indices.delete({
-//   index:'dataset'
-// },function(err,resp){
-//   if(err){
-//     console.log(err);
-//   }
-//   else{
-//     console.log("deleted");
-//     console.log("11111");
-//   }
-// })
-
+//delete indices
+client.indices.delete({
+  index:'dataset'
+},function(err,resp){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("deleted");
+    console.log("11111");
+  }
+})
+// add index 
 async function f1(req,res){
     let temp = await client.ping({
-      // ping usually has a 3000ms timeout
+      
       requestTimeout: Infinity,
-      // undocumented params are appended to the query string
+      
       hello: "elasticsearch!"
     }, function (error) {
       if (error) {
@@ -48,28 +48,6 @@ async function f1(req,res){
     }
    
     
-
-    //   let temp2 = client.index({
-    //     index:'dataset',
-    
-    // },function(err,resp,status){
-    //     if(err){
-    //         console.log(err);
-    //     }
-    //     else{
-    //         console.log("ok");
-    //         console.log("create",resp);
-    //         console.log(dataset[0]);
-    //     }
-    
-    // });
-
-    // if(temp2.err){
-    //   console.log(err)
-    // }
-    // else{
-    //   console.log(3);
-    // }
     for(var i=0;i<dataset.length;i++){
        client.index({
         index:'dataset',
@@ -85,11 +63,11 @@ async function f1(req,res){
 var n = 1200;
     setInterval(function(){
 
-        // f1();
+        f1();
           
       }, n * 1000);  
-// f1();
 
+// check if index exist or not
 client.indices.exists({
   index:'dataset'
 }).then(function(resp){
@@ -98,7 +76,7 @@ client.indices.exists({
 .catch(err=>{
   console.log(err);
 })
-
+// search string 
 async function f2(req,res){
     
     const {temp} = await client.search({  
@@ -132,93 +110,14 @@ async function f2(req,res){
 f2().catch(console.log);
 
 
-// async function run () {
-//   // Let's start by indexing some data
-//   await client.index({
-//     index: 'game-of-thrones',
-//     body: {
-//       character: 'Ned Stark',
-//       quote: 'Winter is coming.'
-//     }
-//   })
 
-//   await client.index({
-//     index: 'game-of-thrones',
-//     body: {
-//       character: 'Daenerys Targaryen',
-//       quote: 'I am the blood of the dragon.'
-//     }
-//   })
 
-//   await client.index({
-//     index: 'game-of-thrones',
-//     // here we are forcing an index refresh,
-//     // otherwise we will not get any result
-//     // in the consequent search
-//     refresh: true,
-//     body: {
-//       character: 'Tyrion Lannister',
-//       quote: 'A mind needs books like a sword needs a whetstone.'
-//     }
-//   })
 
-//   // Let's search!
-//   const { body } = await client.search({
-//     index: 'game-of-thrones',
-//     body: {
-//       query: {
-//         match: {
-//           quote: 'winter'
-//         }
-//       }
-//     }
-//   })
 
-//   console.log(body)
-// }
 
-// run().catch(console.log)
-// client.ping({
-//   // ping usually has a 3000ms timeout
-//   requestTimeout: Infinity,
-//   // undocumented params are appended to the query string
-//   hello: "elasticsearch!"
-// }, function (error) {
-//   if (error) {
-//     console.trace('elasticsearch cluster is down!');
-//   } else {
-//     console.log('All is well');
-//   }
-// });
 
-// articles = [];
-
-// fs.createReadStream('IndianFinancialNews.csv')
-//   .pipe(csv())
-//   .on('data', function (row) {
-//     const art = {
-//       heading : row.Title,
-//       description : row.Description
-//     }
-//     articles.push(art);
-//   })
-//   .on('end', function () {
-//     console.log(articles[0]);
-//   })
  
-// client.indices.create({
-//     index:'names',
 
-// },function(err,resp,status){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("ok");
-//         console.log("create",resp);
-//     }
-
-// });
 
 // router.post('/products',bodyParser,(req,res)=>{
 //     elasticClient.index({
